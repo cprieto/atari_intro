240a6d57031bf0592eaf218d02181b8abb466c0a
Cristian Prieto <me@cprieto.com>
2016-06-09 13:53:24 +0200

    Improved code size

--- helloworld.s
+++ helloworld.s
@@ -13,69 +13,79 @@
     PROCESSOR 6502
     INCLUDE "vcs.h"
 
-    ORG $F000       ; Start of "cart area" (see Atari memory map)
+    ORG $F000
+
+Reset:
+    ldx #0 ; x = 0
+    txs    ; sp = 0, because sp = x
+    pha    ;
+Clear:
+    pha
+    dex
+    bne Clear
 
 StartFrame:
-    lda #%00000010  ; Vertical sync is signaled by VSYNC's bit 1...
+    lda #%00000010
     sta VSYNC
-    REPEAT 3        ; ...and lasts 3 scanlines
-        sta WSYNC   ; (WSYNC write => wait for end of scanline)
-    REPEND
-    lda #0
-    sta VSYNC       ; Signal vertical sync by clearing the bit
-
-PreparePlayfield:   ; We'll use the first VBLANK scanline for setup
-    lda #$00        ; (could have done it before, just once)
-    sta ENABL       ; Turn off ball, missiles and players
-    sta ENAM0
-    sta ENAM1
-    sta GRP0
-    sta GRP1
-    sta COLUBK      ; Background color (black)
-    sta PF0         ; PF0 and PF2 will be "off" (we'll focus on PF1)...
-    sta PF2
-    lda #$FF        ; Playfield collor (yellow-ish)
-    sta COLUPF
-    lda #$00        ; Ensure we will duplicate (and not reflect) PF
-    sta CTRLPF
-    ldx #0          ; X will count visible scanlines, let's reset it
-    REPEAT 37       ; Wait until this (and the other 36) vertical blank
-        sta WSYNC   ; scanlines are finished
-    REPEND
-    lda #0          ; Vertical blank is done, we can "turn on" the beam
-    sta VBLANK
-
-Scanline:
-    cpx #174        ; "HELLO WORLD" = (11 chars x 8 lines - 1) x 2 scanlines =
-    bcs ScanlineEnd ;   174 (0 to 173). After that, skip drawing code
-    txa             ; We want each byte of the hello world phrase on 2 scanlines,
-    lsr             ;   which means Y (bitmap counter) = X (scanline counter) / 2.
-    tay             ;   For division by two we use (A-only) right-shift
-    lda Phrase,y    ; "Phrase,Y" = mem(Phrase+Y) (Y-th address after Phrase)
-    sta PF1         ; Put the value on PF bits 4-11 (0-3 is PF0, 12-15 is PF2)
-ScanlineEnd:
-    sta WSYNC       ; Wait for scanline end
-    inx             ; Increase counter; repeat untill we got all kernel scanlines
-    cpx #191
-    bne Scanline
-
-Overscan:
-    lda #%01000010  ; "turn off" the beam again...
-    sta VBLANK      ;
-    REPEAT 30       ; ...for 30 overscan scanlines...
+    REPEAT 3
         sta WSYNC
     REPEND
-    jmp StartFrame  ; ...and start it over!
+    lda #0
+    sta VSYNC
 
-Phrase:
-    .BYTE %00000000 ; H
+SetColors:
+    lda #$8E
+    sta COLUBK
+    lda #$40
+    sta COLUPF
+
+    ldx #0
+DoVBlank:
+    inx
+    sta WSYNC
+    cpx #37
+    bne DoVBlank
+
+    lda #0
+    sta VBLANK
+
+    ldx #0
+DrawHelloWorld:
+    cpx #174
+    bcs ScanlineEnd
+    txa
+    lsr
+    tay
+    lda Pattern,y
+    sta PF1
+
+ScanlineEnd:
+    sta WSYNC
+    inx
+    cpx #191
+    bne DrawHelloWorld
+
+    lda #%01000010
+    sta VBLANK
+
+    ldx #0
+DoOverscan:
+    sta WSYNC
+    inx
+    cpx #30
+    bne DoOverscan
+
+    jmp StartFrame
+
+Pattern:
+    .BYTE %00000000
     .BYTE %01000010
     .BYTE %01111110
     .BYTE %01000010
     .BYTE %01000010
     .BYTE %01000010
     .BYTE %00000000
-    .BYTE %00000000 ; E
+    .BYTE %00000000
     .BYTE %01111110
     .BYTE %01000000
     .BYTE %01111100
@@ -83,7 +93,7 @@ Phrase:
     .BYTE %01000000
     .BYTE %01111110
     .BYTE %00000000
-    .BYTE %00000000 ; L
+    .BYTE %00000000
     .BYTE %01000000
     .BYTE %01000000
     .BYTE %01000000
@@ -154,10 +164,10 @@ Phrase:
     .BYTE %01000010
     .BYTE %01000100
     .BYTE %01111000
-    .BYTE %00000000 ; Last byte written to PF1 (important, ensures lower tip
-                    ;                           of letter "D" won't "bleeed")
+    .BYTE %00000000
 
-    ORG $FFFA             ; Cart config (so 6502 can start it up)
+
+    ORG $FFFA
 
     .WORD StartFrame      ;     NMI
     .WORD StartFrame      ;     RESET
